742bb9bbcf97f2d4f2f6835b30e4564fed002360
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-10-20 01:14:14 +0530

    Creating Singly Linked List
    program to create Singly Linked List in C

---
+++ Linked List/Singly Linked List/C/Create_Singly_Linked_List.c
@@ -0,0 +1,102 @@
+/*
+ *  Objective  : This Program is to create Singly Linked List.
+ *  Programmer : Jatin Sharma
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int I = 1; // To Print the List using For loop as well as while loop [Global Variable]
+
+void create_linked_list(int n);
+void insert_node_at_first();
+void print_list();
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+node *start = NULL; // Setting Up teh Node to NULL
+
+int main()
+{
+
+    int n;
+    printf("Enter the Number of Total Node you want to insert : ");
+    scanf("%d", &n);
+
+    create_linked_list(n);
+    return 0;
+}
+
+void create_linked_list(int n)
+{
+    node *array, *temp;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        array = (node *)malloc(sizeof(node));
+        if (array == NULL)
+        {
+            printf("Cannot Allocate Memory for Overflow Reason. Please Stop some task and try Again.\n");
+            return;
+        }
+
+        printf("Enter the Data for node [%d] : ", i + 1);
+        scanf("%d", &array->data);
+        array->next = NULL;
+
+        if (start == NULL)
+        {
+            start = array;
+        }
+        else
+        {
+            temp = start;
+
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = array;
+        }
+
+    }
+
+    printf("\n");
+    printf("************************************************************************\n");
+    printf("********************* NODES Created Successfully. *********************\n");
+    printf("************************************************************************\n\n");
+    print_list();
+}
+
+void print_list()
+{
+
+    node *temp = start;
+    if (temp == NULL)
+    {
+        printf("List is Empty.\n");
+        return;
+    }
+
+    /*
+    // You can Use this loop as well
+    for (node *temp ; temp != NULL; temp = temp->next)
+    {
+
+        printf("Data of node [%d] ==> %d\n", I, temp->data);
+        I++;
+    }
+    */
+    while (temp != NULL)
+    {
+        printf("Data of node [%d] ==> %d\n", I, temp->data);
+        I++;
+        temp = temp->next;
+    }
+}
