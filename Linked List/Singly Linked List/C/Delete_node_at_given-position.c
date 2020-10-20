2ab7fb66caf36672f1adf9f37970b8503ddd253a
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-10-20 14:22:49 +0530

    Code Updated.
    Updating Creating Method, Deleting Method, Printing Method as well.

--- Linked List/Singly Linked List/C/Delete_node_at_given-position.c
+++ Linked List/Singly Linked List/C/Delete_node_at_given-position.c
@@ -1,5 +1,5 @@
 /*
- *  Objective  : This Program is to insert the element at the Given/Specific Position in Singly Linked List.
+ *  Objective  : This Program is to insert the node at the Given/Specific Position in Singly Linked List.
  *  Programmer : Jatin Sharma
  */
 
@@ -10,7 +10,7 @@ int I = 1; // To Print the List using For loop as well as while loop [Global Var
 int count = 0;
 
 void create_linked_list(int n);
-void delete_element_at_given();
+void delete_node_at_given();
 void print_list();
 
 typedef struct node
@@ -36,31 +36,35 @@ void create_linked_list(int n)
 {
     node *array, *temp;
 
-    start = (node *)malloc(sizeof(node));
-    if (start == NULL)
-    {
-        printf("Cannot Allocate Memory for Overflow Reason. Please Stop some task and try Again>\n");
-        return;
-    }
-
-    temp = start;
-
-    //It is creating nodes for the data.
     for (int i = 0; i < n; i++)
     {
 
         array = (node *)malloc(sizeof(node));
         if (array == NULL)
         {
-            printf("Cannot Allocate Memory for Overflow Reason. Please Stop some task and try Again>\n");
+            printf("Cannot Allocate Memory for Overflow Reason. Please Stop some task and try Again.\n");
             return;
         }
 
         printf("Enter the Data for node [%d] : ", i + 1);
         scanf("%d", &array->data);
         array->next = NULL;
-        temp->next = array;
-        temp = temp->next;
+
+        if (start == NULL)
+        {
+            start = array;
+        }
+        else
+        {
+            temp = start;
+
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = array;
+        }
         count++;
     }
 
@@ -68,14 +72,46 @@ void create_linked_list(int n)
     printf("************************************************************************\n");
     printf("********************* NODES Inserted Successfully. *********************\n");
     printf("************************************************************************\n\n");
-    delete_element_at_given();
+    delete_node_at_given();
 }
 
-void delete_element_at_given()
+void delete_node_at_given()
 {
 
-
     node *toDelete, *temp;
+
+    if (start == NULL) // Checking isListEmpty
+    {
+        printf("List is Empty.\n");
+        return;
+    }
+    else if (start->next == NULL) // when list only have one node.
+    {
+        char choice[3];
+        printf("List Has only one node. You want to delete it ? (yes/no) : ");
+        scanf("%s", choice);
+
+        if (choice[0] == 'n')
+        {
+            printf("You choosed not to delete the last Node from the list.\n");
+            print_list();
+        }
+
+        else if (choice[0] == 'y')
+        {
+
+            start = NULL;
+            free(start);
+            printf("Only Node Deleted Successfully, Now List is Empty.\n");
+        }
+        else
+        {
+            printf("You Entered Invalid Choice. Please Try Again.\n");
+        }
+        return;
+    }
+
+    //Taking Position to Delete the Node
     int position;
 
     printf("\nEnter the position of the node you want to perform deletion : ");
@@ -88,42 +124,39 @@ void delete_element_at_given()
         return;
     }
 
-    if (start == NULL)      // Checking isListEmpty
+    if (position == 1)
     {
-        printf("List is Empty.\n");
-        return;
+        temp = start;
+        start = temp->next;
+        free(temp);
+        count--;
     }
-    else if (start->next == NULL)       // when list only have one element.
+    else
     {
-        start = NULL;
-        free(start);
-        printf("Node (had one element) Deleted Successfully.\n");
-        return ;
-    }
 
-    toDelete = start;
+        toDelete = start;
 
-    // Loop to the position 
-    for (int i = 0; i < position; i++)
-    {
-        temp = toDelete;
-        toDelete = toDelete->next;
+        // Loop to the position
+        for (int i = 1; i < position; i++)
+        {
+            temp = toDelete;
+            toDelete = toDelete->next;
+        }
+        temp->next = toDelete->next;
+        free(toDelete);
+        count--;
     }
-    temp->next = toDelete->next;
-    free(toDelete);
-    count--;
 
     printf("\n************************************************************************\n");
     printf("******************** Node Deleted from %d Position. ********************\n", position);
     printf("************************************************************************\n\n");
     print_list();
-
 }
 
 void print_list()
 {
-
-    if (start == NULL)
+    node *temp = start;
+    if (temp == NULL)
     {
         printf("List is Empty.\n");
         return;
@@ -131,18 +164,16 @@ void print_list()
 
     /*
     // You can Use this loop as well
-    node *temp =start;
     for (node *temp ; temp != NULL; temp = temp->next)
     {
-
-        printf("Data of node %d = %d\n", I, temp->next->data);
+        printf("Data of node [%d] ==> %d\n", I, temp->data);
         I++;
     }*/
 
-    while (start != NULL)
+    while (temp != NULL)
     {
-        printf("Data of node %d = %d\n", I, start->next->data);
+        printf("Data of node [%d] ==> %d\n", I, temp->data);
         I++;
-        start = start->next;
+        temp = temp->next;
     }
 }
