d096e546f0b37ac1b8a11b2bd58b799aeac33ee5
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-11-09 20:35:37 +0530

    Insert node at Last

---
+++ Linked List/Circular Doubly Linked List/C/Insert_node_at_last.c
@@ -0,0 +1,141 @@
+/*
+    TODO: This Program is to Insert a node at Last position in Circular Doubly Linked List.
+ *  Programmer : Jatin Sharma
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int I = 1;
+void createLinkedList(int n);
+void lastInsert(int element);
+void printLinkedList();
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+} node;
+
+node *start = NULL;         // Setting start to NUll
+node *last = NULL;
+
+int main()
+{
+
+    int n;
+    printf("Enter the Number of Total Node you want to insert : ");
+    scanf("%d", &n);
+
+    createLinkedList(n);
+    int element;
+    printf("Enter the Data to Insert at First Position : ");
+    scanf("%d", &element);
+
+    lastInsert(element); // Calling insert Function with element argument
+    printLinkedList(); // Printing Circular Doubly Linked List.
+    return 0;
+}
+
+void createLinkedList(int n)
+{
+    node *ptr, *temp;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        ptr = (node *)malloc(sizeof(node));
+        if (ptr == NULL)
+        {
+            printf("Cannot Allocate Memory for Overflow Reason. Please Stop some task and try Again.\n");
+            return;
+        }
+
+        printf("Enter the Data for node [%d] : ", i + 1);
+        scanf("%d", &ptr->data);
+
+        if (start == NULL)
+        {
+            start = ptr;
+            ptr->next = start;
+            ptr->prev = start;
+            last = ptr;
+        }
+        else
+        {
+            ptr->next = start;
+            ptr->prev = temp;
+            last->next = ptr;
+            last = ptr;
+            start->prev = last;
+        }
+    }
+
+    printf("\n************************************************************************\n");
+    printf("********************* NODES Created Successfully. *********************\n");
+    printf("************************************************************************\n\n");
+}
+
+void lastInsert(int element)
+{
+    node *ptr, *temp;
+    ptr = (node *)malloc(sizeof(node));
+
+    if (ptr == NULL)
+    {
+        printf("Cannot Allocate Memory for Overflow Reason. Please Stop some task and try Again.\n");
+        return;
+    }
+
+    ptr->data = element;
+
+    if (start == NULL)
+    {
+        ptr->prev = NULL;
+        ptr->next = NULL;
+        start = ptr;
+        last = ptr;
+    }
+    else
+    {
+        ptr->next = start;
+        ptr->prev = temp;
+        last->next = ptr;
+        last = ptr;
+        start->prev = last;
+    }
+
+    printf("\n************************************************************************\n");
+    printf("******************** Data %d Inserted Successfully. ********************\n", element);
+    printf("************************************************************************\n\n");
+}
+
+void printLinkedList()
+{
+
+    node *temp = start;
+    if (temp == NULL)
+    {
+        printf("List is Empty.\n");
+        return;
+    }
+
+    /*
+    // You can Use this loop as well
+    for (node *temp ; temp->next != start; temp = temp->next)
+    {
+        printf("Data of node [%d] ==> %d\n", I, temp->data);
+        I++;
+    }
+    printf("Data of node [%d] ==> %d\n", I, temp->data);
+
+    */
+    while (temp->next != start)
+    {
+        printf("Data of node [%d] ==> %d\n", I, temp->data);
+        I++;
+        temp = temp->next;
+    }
+    printf("Data of node [%d] ==> %d\n", I, temp->data);
+}
